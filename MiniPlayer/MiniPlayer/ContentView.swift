e23f93b26e8b4b5aeb1253aa3c67f226343855ff
AdrianSA <50012235+cloner69@users.noreply.github.com>
2025-02-03 13:23:48 +0700

    Mini Video Player feature

---
+++ MiniPlayer/MiniPlayer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MiniPlayer
+//
+//  Created by Adrian Suryo Abiyoga on 20/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MiniPlayer/MiniPlayer/MiniPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniPlayerApp.swift
+//  MiniPlayer
+//
+//  Created by Adrian Suryo Abiyoga on 20/01/25.
+//
+
+import SwiftUI
+
+@main
+struct MiniPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
